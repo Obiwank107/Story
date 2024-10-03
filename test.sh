91aa95bf8bdbacaf7ac749daa89f5c709a30b614
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 18:02:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 # Prompt for user input
 while true; do
     read -e -p "Enter your moniker name: " MONIKER_NAME
+    sleep 5
     if [[ -z "$MONIKER_NAME" ]]; then
         echo "Moniker name cannot be empty. Please enter a valid name."
     else
