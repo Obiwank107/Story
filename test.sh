926a912b8d5839dab4ce2ae9a06592dd3d4369d5
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:25:54 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,7 @@
 
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
-
-store_operator_fid_env() {
-    local MONIKER_NAME
-    read -p "> Your Moniker Name: " MONIKER_NAME
-}
+read -p "> Your Moniker Name: " MONIKER_NAME
 
 echo "Your moniker name is: $MONIKER_NAME"
 sleep 3
@@ -14,6 +10,4 @@ sleep 3
 #System Upgrade and Install
 echo "Upgrading and installing dependencies, if needed"
 sleep 3
-sudo apt update && sudo apt upgrade -y || { echo "Failed to upgrade system"; exit 1; }
-sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y || { echo "Failed to install dependencies"; exit 1; }
 echo "${GREEN}✅ Upgrading and installing dependencies Completed${RESET}"
