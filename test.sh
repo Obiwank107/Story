775d35e3521d2afb10cf1f83a56a4de44629631a
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:02:04 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
+#User input Monikor Name
+# Clear the MONIKER_NAME variable
+unset MONIKER_NAME
+echo "Input your Moniker Name then Press Enter"
+read -p "Enter your moniker name: " MONIKER_NAME
+
+sleep 10
+
+echo "Your moniker name is: $MONIKER_NAME"
+sleep 3
+
+#System Upgrade and Install
+echo "Upgrading and installing dependencies, if needed"
+sleep 3
+sudo apt update && sudo apt upgrade -y || { echo "Failed to upgrade system"; exit 1; }
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y || { echo "Failed to install dependencies"; exit 1; }
+echo "${GREEN}✅ Upgrading and installing dependencies Completed${RESET}"
