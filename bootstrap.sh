179fed7da584bfd6c9ba395b6959554b02bd2063
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 16:24:40 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
 #User input Monikor Name
 echo "Input your Moniker Name then Press Enter"
-read -p "Enter your moniker name: " MONIKER_NAME
-
-sleep 1
-echo "Your Monikor Name is $MONIKER_NAME"
+# Check if MONIKER_NAME is passed as an environment variable, otherwise prompt the user
+if [ -z "$MONIKER_NAME" ]; then
+    read -p "Enter your moniker name: " MONIKER_NAME
+fi
+echo "Your moniker name is $MONIKER_NAME"
 sleep 3
 #System Upgrade and Install
 echo "Upgrading and installing dependencies, if needed"
 sleep 3
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
-echo "✅ Upgrading and installing dependencies Completed"
+sudo apt update && sudo apt upgrade -y || { echo "Failed to upgrade system"; exit 1; }
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y || { echo "Failed to install dependencies"; exit 1; }
+echo "${GREEN}✅ Upgrading and installing dependencies Completed${RESET}"
 sleep 3
 #Install Go
 echo "Installing Go..."
 sleep 3
 cd $HOME
 VER="1.23.1"
+GO_URL="https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+if curl --output /dev/null --silent --head --fail "$GO_URL"; then
+    echo "Go download URL reachable."
+else
+    echo "Go download URL not reachable, please check the URL or network connection."
+    exit 1
+fi
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
@@ -28,7 +37,7 @@ echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
 source $HOME/.bash_profile
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 go version
-echo "✅ Install GO Completed"
+echo "${GREEN}✅ Install GO Completed${RESET}"
 sleep 3
 #Downloading and installing Story-Geth binary
 echo "Downloading and installing Story-Geth binary..."
@@ -44,7 +53,7 @@ fi
 sudo cp geth-linux-amd64-0.9.3-b224fdf/geth $HOME/go/bin/story-geth
 source $HOME/.bash_profile
 story-geth version
-echo "✅ Install story-geth binary Completed"
+echo "${GREEN}✅ Install story-geth binary Completed${RESET}"
 sleep 3
 #Build Story Binary
 echo "Building Story binary(Latest Version)"
@@ -65,13 +74,13 @@ fi
 sudo mv ~/story/story ~/go/bin/
 source $HOME/.bash_profile
 story version
-echo "✅ Install story binary Completed"
+echo "${GREEN}✅ Install story binary Completed${RESET}"
 sleep 3
 #Initialize the Iliad Network Node
 echo "Initializing Iliad Network Node Moniker Name : $MONIKER_NAME"
 sleep 3
 story init --network iliad --moniker "$MONIKER_NAME"
-echo "✅ Network iliad $MONIKER_NAME initialize Completed"
+echo "${GREEN}✅ Network iliad $MONIKER_NAME initialize Completed${RESET}"
 sleep 3
 #Create and Configure systemd Service for Story-Geth
 echo "Creating systemd service for Story-Geth..."
@@ -91,7 +100,7 @@ LimitNOFILE=4096
 [Install]
 WantedBy=multi-user.target
 EOF
-echo "✅ Create systemd story-geth.service Completed"
+echo "${GREEN}✅ Create systemd story-geth.service Completed${RESET}"
 sleep 3
 #Create and Configure systemd Service for Story
 echo "Creating systemd service for Story..."
@@ -111,7 +120,7 @@ LimitNOFILE=4096
 [Install]
 WantedBy=multi-user.target
 EOF
-echo "✅ Create systemd story.service Completed"
+echo "${GREEN}✅ Create systemd story.service Completed${RESET}"
 sleep 3
 #Reload systemd, Enable, and Start Services
 echo "Reloading systemd, enabling, and starting Story-Geth and Story services..."
@@ -121,7 +130,7 @@ sudo systemctl enable story-geth story
 sudo systemctl enable story
 sudo systemctl start story-geth story
 sudo systemctl start story
-echo "✅ Start story and story-geth service Completed"
+echo "${GREEN}✅ Start story and story-geth service Completed${RESET}"
 sleep 3
 #Check Service Status
 echo "Checking Story-Geth service status..."
@@ -137,7 +146,7 @@ echo "Checking sync status..."
 sleep 3
 curl -s localhost:26657/status | jq
 sleep 3
-echo "✅✅✅✅✅ Congrats! Installation and setup completed! ✅✅✅✅✅"
+echo "${GREEN}✅✅✅✅✅ Congrats! Installation and setup completed! ✅✅✅✅✅${RESET}"
 sleep 1
 echo "Command to check Story logs >> sudo journalctl -u story -f -o cat <<"
 sleep 1
