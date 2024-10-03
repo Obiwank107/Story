7d2159d8c6ca9f4a11a67afc0bd0d9067f9b77c3
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:42:11 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 # Function to read user input for moniker name
-get_moniker_name() {
     read -p "Enter your moniker name: " MONIKER_NAME
     echo "Your moniker name is: $MONIKER_NAME"
     sleep 3
-}
