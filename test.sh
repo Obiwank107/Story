343be87f79b5013f5209d08e4c5276b033589991
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:55:19 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Function to read user input for moniker name
-    echo "Enter your moniker name:"
-    read -p moniker
+
+while true; do
+    read -e -p "Enter your moniker name: " MONIKER_NAME
+    if [[ -z "$MONIKER_NAME" ]]; then
+        echo "Moniker name cannot be empty. Please enter a valid name."
+    else
+        break
+    fi
+done
+
+echo "Your moniker name is: $MONIKER_NAME"
