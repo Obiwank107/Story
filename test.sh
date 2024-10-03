d3d4df0407f02a911eaa0c43e9f912c426c8359f
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:31:00 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,3 @@ GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
 read -p "> Your Moniker Name: " MONIKER_NAME
-echo "Your moniker name is: $MONIKER_NAME"
-sleep 3
-
-#System Upgrade and Install
-echo "Upgrading and installing dependencies, if needed"
-sleep 3
-echo "${GREEN}✅ Upgrading and installing dependencies Completed${RESET}"
