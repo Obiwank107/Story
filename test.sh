ef0baf4c1e71a7905702cae6df76c6fc3094d7fd
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-03 17:41:23 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
-GREEN=$(tput setaf 2)
-RESET=$(tput sgr0)
-
 # Function to read user input for moniker name
 get_moniker_name() {
     read -p "Enter your moniker name: " MONIKER_NAME
     echo "Your moniker name is: $MONIKER_NAME"
     sleep 3
 }
+#!/bin/bash
+
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
