c757d24350301e3eb34220f9250d366dccdcaaba
Obiwank107 <105133800+Obiwank107@users.noreply.github.com>
2024-10-08 17:38:38 +0700

    Update autoremove.sh

--- autoremove.sh
+++ autoremove.sh
@@ -6,16 +6,16 @@ echo -e "\e[33m🚨 This action CANNOT be undone! If you wish to cancel, PRESS C
 # Add a short sleep to give time for the user to cancel
 sleep 5
 
-sudo systemctl stop story-geth
-sudo systemctl stop story
-sudo systemctl disable story-geth
-sudo systemctl disable story
-sudo rm /etc/systemd/system/story-geth.service
-sudo rm /etc/systemd/system/story.service
+sudo systemctl stop story && \
+sudo systemctl stop story-geth && \
+sudo systemctl disable story-geth && \
+sudo systemctl disable story && \
+sudo rm $HOME/go/bin/story-geth && \
+sudo rm $HOME/go/bin/story && \
+sudo rm -rf $HOME/.story && \
+sudo rm /etc/systemd/system/story-geth.service && \
+sudo rm /etc/systemd/system/story.service && \
 sudo systemctl daemon-reload
-sudo rm -rf $HOME/.story
-sudo rm $HOME/go/bin/story-geth
-sudo rm $HOME/go/bin/story
 
 echo -e "\e[32m🎉 CONGRATULATIONS! 🎉\e[0m"
 echo -e "\e[32m✅ Story Node has been successfully removed from your device! ✅\e[0m"
